d0d8fc8aca7ef445b204891b31b4b7d5259b0289
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 00:01:49 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/mapintegration/main.go
@@ -0,0 +1,23 @@
+// cmd/mapintegration/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "mapintegration/internal/mapintegration"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := mapintegration.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
